8e7e70eb32e5516b0a4d6f3b65d11bb93b490bc0
Sidlmao <135448786+Sidlmao@users.noreply.github.com>
2024-06-10 19:00:29 -0400

    Update dec.h

--- dec.h
+++ dec.h
@@ -1,14 +1,15 @@
+//header guard
 #ifndef DEC_H
 #define DEC_H
 
-void introduction();
-bool is_winner();
+void introduction(); // Prints introduction
+bool is_winner(); // Checks if the game is over
 bool filled_up();   //filled_up()
-void draw();
-void set_position();
-void update_board();
-void change_player();
-void take_turn();
-void end_game();
+void draw();//checks for draw
+void set_position(); //sets the position
+void update_board();//updates after each turn
+void change_player();//switchs player
+void take_turn();//takes turn
+void end_game();//ends the game
 
-#endif
+#endif //end of header gaurd
