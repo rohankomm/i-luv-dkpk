ef377b1462165613d6295421377dea2622d7b986
Sidlmao <135448786+Sidlmao@users.noreply.github.com>
2024-06-10 19:00:51 -0400

    Update .main.cpp

--- .main.cpp
+++ .main.cpp
@@ -3,8 +3,11 @@
 using namespace std;
 int main()
 {
-	introduction();
-	take_turn();
-	end_game();
-	return 0;
+  //displays introduction to the game
+  introduction();
+  //starts the game, taking turns until someone wins or board is filled up
+  take_turn();
+  //end the game and display the result
+  end_game();
+  return 0;
 }
