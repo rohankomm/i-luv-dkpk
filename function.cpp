721f25806d4c53846c213dd60e01c09f1d7a5dc3
DhruvaKPK <74214583+DhruvaKPK@users.noreply.github.com>
2024-06-10 20:47:23 -0400

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -1,40 +1,52 @@
 #include <iostream>
 #include "dec.h"
+#include <stdlib.h>
+#include <string> //Used to convert the set color macro to a string
 //globally defined
 using namespace std;
+
+#define RESET_COLOR "\033[0m" 
+//Set Color Macro
+#define SET_COLOR(color) "\033[" + std::string(color)
+//Set Color Macro
+
 string board[9] = {" ", " ", " ", " ", " ", " ", " ", " ", " "}; 
 int player = 1; //represents current player  
 int position = 0;  //represents position chosen by the player
-
 //displays introduction to the game
 void introduction()
 {
+  cout << SET_COLOR("\033[35m"); // Set text color to magenta
   cout<<"Press [Enter] to begin: ";
   getchar(); //haults the screen  
   cout<<"\n";
 
    cout<<"\n";
   //headers
-  cout<<"***********\n";
-  cout<<"Tic-Tac-Toe\n";
-  cout<<"***********\n";
+  cout << SET_COLOR("\033[37m"); // Set text color to white
+  cout<< SET_COLOR("\033[30m") << "***********\n";
+  cout<<SET_COLOR("\033[37m")  << "Tic-Tac-Toe\n";
+  cout<< SET_COLOR("\033[30m") <<"***********\n";
 
-  cout<<"Player 1) X\n";
-  cout<<"Player 2) O\n";
-
+  cout<< SET_COLOR("\033[34m") << "Player 1) X\n";
+  cout<< SET_COLOR("\033[31m") << "Player 2) O\n";
+cout << SET_COLOR("\033[37m");
   
 //displays grid
   cout<<"The 3x3 grid is shown below:\n\n";
 
   cout << "     |     |      \n";
-  cout << "  1  |  2  |  3   \n";
+  cout << "  "<< SET_COLOR("\033[35m") << "1  " << SET_COLOR("\033[37m") << "|  " << SET_COLOR("\033[35m") << "2  " << SET_COLOR("\033[37m") << "|  " << SET_COLOR("\033[35m") << "3   \n";
+  cout << SET_COLOR("\033[37m"); // Set text color to white
   cout << "_____|_____|_____ \n";
   cout << "     |     |      \n";
-  cout << "  4  |  5  |  6   \n";
+  cout << "  "<< SET_COLOR("\033[35m") << "4  " << SET_COLOR("\033[37m") << "|  " << SET_COLOR("\033[35m") << "5  " << SET_COLOR("\033[37m") << "|  " << SET_COLOR("\033[35m") << "6   \n";
+  cout << SET_COLOR("\033[37m"); // Set text color to white
   cout << "_____|_____|_____ \n";
   cout << "     |     |      \n";
-  cout << "  7  |  8  |  9   \n";
-  cout << "     |     |      \n\n";
+  cout << "  "<< SET_COLOR("\033[35m") << "7  " << SET_COLOR("\033[37m") << "|  " << SET_COLOR("\033[35m") << "8  " << SET_COLOR("\033[37m") << "|  " << SET_COLOR("\033[35m") << "9   \n"; 
+  cout << SET_COLOR("\033[37m"); // Set text color to white
+cout << "     |     |      \n\n";
 
 }
 
@@ -118,6 +130,7 @@ void draw()
   cout << "     |     |      \n";
   cout << "  " << board[3] << "  |  " << board[4] << "  |  " << board[5] << "\n";
   cout << "_____|_____|_____ \n";
+  cout << "     |     |      \n";
   cout << "  " << board[6] << "  |  " << board[7] << "  |  " << board[8] << "\n";
   cout << "     |     |      \n";
   cout << "\n";
@@ -128,7 +141,7 @@ void draw()
 
 void set_position()
 {
-  cout<<"Player "<<player<<"'s turn (Enter: 1-9)";  //getting user input
+  cout<<"Player "<<player<<"'s turn (Enter: 1-9): ";  //getting user input
   while(!(cin>>position))
   {
     cout<<"Please enter a valid number between (1-9)"<<endl; //error trap
@@ -154,11 +167,12 @@ void update_board()
 {
 
   if (player % 2 == 1){
-    board[position - 1] = "X"; // mark player ones move
+    board[position - 1] = SET_COLOR("\033[34m") + "X" + SET_COLOR("\033[37m"); // mark player ones move
+  
   }
   else
   {
-    board[position - 1] = "O"; // mark player twos move
+    board[position - 1] = SET_COLOR("\033[31m") + "O" + SET_COLOR("\033[37m"); // mark player twos move
   }
 }
 
@@ -174,7 +188,7 @@ void change_player() {
 
 //function that executes each turn of the game
 void take_turn()
-{
+{ 
   while(!is_winner() && !filled_up())  
   {
     //invoking the functions for necessary turns
@@ -190,12 +204,14 @@ void end_game()
 {
   if(is_winner())
   {
-    cout<<"There is a winner!"<<endl; //winner message
+    cout<< SET_COLOR("\033[33m") << "There is a winner!"<<endl; //winner message
+    cout << SET_COLOR("\033[37m");
 
   }
 
   else if(filled_up())
   {
-    cout<<"There is a tie!"<<endl; //tie message
+    cout<< SET_COLOR("\033[36m") << "There is a tie!"<<endl; //tie message
+    cout << SET_COLOR("\033[37m");
   }
 }
